d545d57f9edabff483643753e72837604b68bdf7
pengjungang <pjg202311@gmail.com>
2024-10-16 19:39:14 +0800

    code optimization

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 const GLuint WIDTH = 800, HEIGHT = 600;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
-int main()
+static GLFWwindow* initWindow()
 {
     // init glfw
     glfwInit();
@@ -16,26 +16,44 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* windows = glfwCreateWindow(WIDTH, HEIGHT, "Example", NULL, NULL);
-    glfwMakeContextCurrent(windows);
-    if (windows == NULL)
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL)
     {
         std::cout << "glfw create windows failed" << std::endl;
-        return -1;
+        return NULL;
     }
+    return window;
+}
 
-    glfwSetKeyCallback(windows, key_callback);
-
+static int loadGLFunc()
+{
     int version = gladLoadGL(glfwGetProcAddress);
     if (version == 0)
     {
         std::cout << "glad load opengl function failed" << std::endl;
         return -1;
     }
+    return 0;
+}
+
+int main()
+{
+    GLFWwindow* window = initWindow();
+    if (window == NULL)
+    {
+        return -1;
+    }
+    glfwSetKeyCallback(window, key_callback);
+
+    if (loadGLFunc() == -1)
+    {
+        return -1;
+    }
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
-    float vertexPositons[] = {
+    float vertexPositions[] = {
         0.0F,
         0.5F,
         0.0F,
@@ -53,7 +71,7 @@ int main()
     GLuint positionBufferObject = 0;
     glGenBuffers(1, &positionBufferObject);
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositons), vertexPositons, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glEnableVertexAttribArray(0);
@@ -77,14 +95,14 @@ int main()
             outColor = vec4(1.0, 1.0, 1.0, 1.0); 
         } 
     )";
-    while (glfwWindowShouldClose(windows) == 0)
+    while (glfwWindowShouldClose(window) == 0)
     {
         glfwPollEvents();
 
         glClearColor(0.2F, 0.3F, 0.3F, 1.0F);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(windows);
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
@@ -93,6 +111,6 @@ int main()
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    // TODO(pjg): 
+    // TODO(pjg):
     std::cout << "input key: " << key << std::endl;
 }
