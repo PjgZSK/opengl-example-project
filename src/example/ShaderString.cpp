1223dca1843ab0d425524124d29a3508ae577dc4
pengjungang <pjg202311@gmail.com>
2024-10-22 17:45:09 +0800

    draw triangle

--- src/example/ShaderString.cpp
+++ src/example/ShaderString.cpp
@@ -7,7 +7,12 @@ const GLchar* const ShaderString::dot_vert_shader =
 
 void main(void)
 {
-    gl_Position = vec4(0.0, 0.0, 0.5, 1.0);
+    const vec4 vertices[3] = vec4[3](
+        vec4(-0.5, -0.5, 0.5, 1.0),
+        vec4(0.5, 0.5, 0.5, 1.0),
+        vec4(0.5, -0.5, 0.5, 1.0)
+    );
+    gl_Position = vertices[gl_VertexID];
 }
 )";
 
