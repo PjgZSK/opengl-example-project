6f23bd013893c6242458d06ad972422491662b12
pengjungang <pjg202311@gmail.com>
2024-10-22 16:42:01 +0800

    OpenGL version 330 -> 430

--- src/example/ShaderString.cpp
+++ src/example/ShaderString.cpp
@@ -3,9 +3,9 @@
 
 const GLchar* const ShaderString::dot_vert_shader =
     R"(
-#version 330
+#version 430 core
 
-void main()
+void main(void)
 {
     gl_Position = vec4(0.0, 0.0, 0.5, 1.0);
 }
@@ -13,11 +13,11 @@ void main()
 
 const GLchar* const ShaderString::dot_frag_shader =
     R"(
-#version 330
+#version 430 core
 
 out vec4 color;
 
-void main()
+void main(void)
 {
     color = vec4(0.0, 0.8, 1.0, 1.0);
 }
